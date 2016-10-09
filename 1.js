3e32f8ccf780e8585b3de34135b88dc6be92429d
nizp <nzp890524@163.com>
2016-10-09 14:22:13 +0800

    nizp2

--- 1.js
+++ 1.js
@@ -5,21 +5,21 @@ window.onload = function(){
 	}
 	
 	Tab.prototype.init = function(){
-		var that = this;
 		for(var i=0;i<this.btn.length;i++){
-			this.btn[i].index = i;
-			this.btn[i].onclick = function(){
-				that.fn(this);
-			}
+			(function(index){
+				this.btn[i].onclick = function(){
+					that.fn(this.index);
+				}
+			})(i);
 		}
 	}
-	Tab.prototype.fn = function(that){
+	Tab.prototype.fn = function(index){
 		for(var i=0;i<this.btn.length;i++){
 			this.btn[i].className = '';
 			this.divs[i].className = '';
 		}
-		that.className = 'active';
-		this.divs[that.index].className = 'show';
+		this.btn[index].className = 'active';
+		this.divs[index].className = 'show';
 		
 	}
 }
