ba5f8eba82ac4ef5d0d74d595557bb75846f9414
nizp <nzp890524@163.com>
2016-10-09 10:15:26 +0800

    addjs

--- 1.js
+++ 1.js
@@ -3,4 +3,14 @@ window.onload = function(){
 		this.divs = document.getElementsByTagName('div');
 		this.btn = document.getElementsByTagName('input');
 	}
+	
+	Tab.prototype.init = function(){
+		var that = this;
+		for(var i=0;i<this.btn.length;i++){
+			this.btn[i].index = i;
+			this.btn[i].onclick = function(){
+				
+			}
+		}
+	}
 }
